ce26a3d053bbedf6d9a845c3fbb0e5c223299505
davidlegare <davidlegare01@gmail.com>
2018-11-10 15:18:47 -0500

    Changes to client

--- src/client/AddContactController.java
+++ src/client/AddContactController.java
@@ -1,9 +1,6 @@
 package client;
 
-import client.ClientInfo;
 import dbUtil.DBConnection;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,89 +9,85 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddContactController implements Initializable{
-	
-	@FXML private TextField name;
-	@FXML private TextField industryType;
-	@FXML private TextField parentCompany;
-	@FXML private TextField status;
-	@FXML private TextField fee;
 
-	private DBConnection DBConn;
-	private ObservableList<ClientInfo> data;
-	private FilteredList filter;
+	@FXML BorderPane addContactPane;
+
+	@FXML private TextField textFname;
+	@FXML private TextField textLname;
+	@FXML private TextField textTitle;
+	@FXML private TextField textDepartment;
+	@FXML private TextField textLanguage;
 
 	@FXML TextField textPhoneName;
 	@FXML TextField textPhoneInput;
 	@FXML GridPane gridPhoneNumber;
 	int gridPhoneRow = 0;
 
+	HashMap hashPhoneName = new HashMap();
+	HashMap hashPhoneInput = new HashMap();
+
 	@FXML TextField textEmailName;
 	@FXML TextField textEmailInput;
 	@FXML GridPane gridEmail;
 	int gridEmailRow = 0;
 
-	private String clientInsert = "INSERT INTO Clients(name,industry,parentCompany,status,fee) VALUES (?,?,?,?,?)";
+	HashMap hashEmailName = new HashMap();
+	HashMap hashEmail = new HashMap();
+
+	private String contactInsert = "INSERT INTO ClientContact(fname,lname,title,department,language) VALUES (?,?,?,?,?)";
+	private String contactInfoInsert = "INSERT INTO ContactInfo(contactID,contactType,infoType,info) VALUES (?,?,?,?)";
+
+	private int clientID = 0;
+	private String contactType = "Client";
+	private String infoType = "";
 	
 	public void initialize(URL url, ResourceBundle rb) {
 		
 	}
-	
-	@FXML
-	private void saveClient(ActionEvent event) throws SQLException {
-		if (confirmSave()) {
-			Connection conn = DBConnection.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(clientInsert); 
-			
-			stmt.setString(1, this.name.getText());
-			stmt.setString(2, this.industryType.getText());
-			stmt.setString(3, this.parentCompany.getText());
-			stmt.setString(4, this.status.getText());
-			stmt.setString(5, this.fee.getText());
-			
-			stmt.execute();
-			conn.close();
-			
-			this.name.setText("");
-			this.industryType.setText("");
-			this.parentCompany.setText("");
-			this.status.setText("");
-			this.fee.setText("");			
-		}		
-	}
-	
-	private boolean confirmSave() {
-		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Confirmation");
-		alert.setHeaderText(null);
-		alert.setContentText("Are you sure?");
-
-		ButtonType btnConfirm = new ButtonType("Confirm");
-		ButtonType btnCancel = new ButtonType("Cancel");
-
-		alert.getButtonTypes().setAll(btnConfirm, btnCancel);
-		Optional<ButtonType> result = alert.showAndWait();
-		
-		if (result.get() == btnCancel) {
-			
-			return false;
-		}
-		    
-		return true;
-	}
 
 	@FXML
 	private void saveContact(ActionEvent e){
+		try {
+			Connection conn = DBConnection.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(contactInsert);
 
+			stmt.setString(1, textFname.getText());
+			stmt.setString(2, textLname.getText());
+			stmt.setString(3, textTitle.getText());
+			stmt.setString(4, textDepartment.getText());
+			stmt.setString(5, textLanguage.getText());
+
+			stmt.execute();
+			conn.close();
+
+		}catch (SQLException eSQL){
+			System.err.println(eSQL);
+		}
+
+		clientID = getClientID();
+
+		if (!gridPhoneNumber.getChildren().isEmpty())
+			savePhoneNumbers();
+
+		if (!gridEmail.getChildren().isEmpty())
+			saveEmails();
+
+		closePage();
 	}
 
 	@FXML
@@ -107,6 +100,9 @@ public class AddContactController implements Initializable{
 
 		gridPhoneNumber.add(lblPhoneName, 0, gridPhoneRow);
 		gridPhoneNumber.add(lblPhoneInput, 1, gridPhoneRow);
+
+		textPhoneName.setText("");
+		textPhoneInput.setText("");
 
 		gridPhoneRow++;
 	}
@@ -122,6 +118,82 @@ public class AddContactController implements Initializable{
 		gridEmail.add(lblEmailName, 0, gridEmailRow);
 		gridEmail.add(lblEmailInput, 1, gridEmailRow);
 
+		textEmailName.setText("");
+		textEmailInput.setText("");
+
 		gridEmailRow++;
+	}
+
+	private void savePhoneNumbers(){
+		infoType = "PhoneNumber";
+
+		try {
+
+			Connection conn = DBConnection.getConnection();
+
+			for (int i = 0; i < gridPhoneRow*2; i++) {
+				PreparedStatement stmt = conn.prepareStatement(contactInfoInsert);
+
+				stmt.setInt(1, clientID);
+				stmt.setString(2, contactType);
+				stmt.setString(3, infoType);
+
+				try {
+					stmt.setString(4, gridPhoneNumber.getChildren().get((i * (gridPhoneRow + 1)) + 2).toString());
+				}catch(Exception e){
+
+				}
+				stmt.execute();
+			}
+			conn.close();
+		}catch (SQLException eSQL){
+			System.err.println(eSQL);
+		}
+	}
+
+	private void saveEmails(){
+		infoType = "Email";
+
+		try {
+
+			Connection conn = DBConnection.getConnection();
+
+			for (int i = 0; i < gridEmailRow*2; i++) {
+				PreparedStatement stmt = conn.prepareStatement(contactInfoInsert);
+
+				stmt.setInt(1, clientID);
+				stmt.setString(2, contactType);
+				stmt.setString(3, infoType);
+				stmt.setString(4, gridEmail.getChildren().get((i*(gridEmailRow+1))+2).toString());
+
+				stmt.execute();
+			}
+			conn.close();
+
+		}catch (SQLException eSQL){
+			System.err.println(eSQL);
+		}
+	}
+
+	private int getClientID(){
+		int clientID = 0;
+
+		try{
+			Connection conn = DBConnection.getConnection();
+			String clientIDQuery = "SELECT id from Clients ORDER BY id DESC";
+
+			ResultSet rs = conn.createStatement().executeQuery(clientIDQuery);
+			clientID = rs.getInt(1);
+
+			conn.close();
+		}catch(SQLException e){
+			System.err.println();
+		}
+		return clientID;
+	}
+
+	private void closePage(){
+		Stage stage = (Stage) addContactPane.getScene().getWindow();
+		stage.close();
 	}
 }
